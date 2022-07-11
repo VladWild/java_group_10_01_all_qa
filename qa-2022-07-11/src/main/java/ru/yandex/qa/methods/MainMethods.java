341d9f2878a958acd51824df3182a9a2ecd21051
VladWild <vladwild220@gmail.com>
2022-07-11 08:40:36 +0300

    qa-2022-07-11 switch and methods

--- qa-2022-07-11/src/main/java/ru/yandex/qa/methods/MainMethods.java
+++ qa-2022-07-11/src/main/java/ru/yandex/qa/methods/MainMethods.java
@@ -3,6 +3,81 @@ package ru.yandex.qa.methods;
 public class MainMethods {
 
     public static void main(String[] args) {
+        print();
+        print("Привет! Как дела?");
+        print("Привет! Сколько тебе лет?");
+        print("Привет! Мне ", 27);
+        print(null);
 
+        System.out.println("-------------------------");
+        String begin = getBegin("Привет! Как дела?", 5);
+        System.out.println(begin);
+
+        System.out.println("-------------------------");
+        StringInfo beginAndEnd = getBeginAndEnd("Привет! Как дела?", 5);
+        System.out.println(beginAndEnd.getBegin());
+        System.out.println(beginAndEnd.getEnd());
+    }
+
+    private static String getBegin(String s, int count) {
+        String substring = s.substring(0, count);
+        return substring;
+    }
+
+    private static StringInfo getBeginAndEnd(String s, int count) {
+        String begin = s.substring(0, count);
+        String end = s.substring(s.length() - count);
+        return new StringInfo(begin, end);
+    }
+
+    private static void print(String s, int i) {
+        System.out.printf("%s%s%n", s, i);
+    }
+
+    private static void print(String str) {
+        System.out.println(str);
+    }
+
+    //прикол - java отдаст предпочтение более низкоуровнему классу string
+    private static void print(Object obj) {
+        System.out.println("Object " + obj);
+    }
+
+    private static void print() {
+        System.out.println("Привет!");
+    }
+
+    private static class StringInfo {
+        private String begin;
+        private String end;
+
+        public StringInfo(String begin, String end) {
+            this.begin = begin;
+            this.end = end;
+        }
+
+        public String getBegin() {
+            return begin;
+        }
+
+        public void setBegin(String begin) {
+            this.begin = begin;
+        }
+
+        public String getEnd() {
+            return end;
+        }
+
+        public void setEnd(String end) {
+            this.end = end;
+        }
+
+        @Override
+        public String toString() {
+            return "StringInfo{" +
+                    "begin='" + begin + '\'' +
+                    ", end='" + end + '\'' +
+                    '}';
+        }
     }
 }
--- qa-2022-07-11/src/main/java/ru/yandex/qa/tz1/MainSwitch.java
+++ qa-2022-07-11/src/main/java/ru/yandex/qa/tz1/MainSwitch.java
@@ -1,8 +1,91 @@
 package ru.yandex.qa.tz1;
 
+import java.util.Scanner;
+
 public class MainSwitch {
 
     public static void main(String[] args) {
+        //число
+        //exampleInt();
 
+        //строка
+        //exampleString();
+
+        //Енам
+        //exampleEnum();
+    }
+
+    private static void exampleEnum() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Введите цвет:");
+        String colorStr = scanner.nextLine();
+        Color color = getColor(colorStr);
+
+        switch (color) {
+            case RED -> System.out.println("Вы ввели красный цвет");
+            case BLUE, GREEN -> System.out.println("Вы ввели синий или зеленый цвет");
+            default -> System.out.println("Неизвестный цвет");
+        }
+
+        scanner.close();
+    }
+
+    private static Color getColor(String colorStr) {
+        Color color = null;
+        try {
+            color = Color.valueOf(colorStr);
+        } catch (IllegalArgumentException ex) {
+            System.out.println("Ошибка распознавания цвета");
+            color = Color.UNKNOWN;
+        }
+        return color;
+    }
+
+    private static enum Color {
+        BLUE, RED, GREEN, UNKNOWN;
+    }
+
+    private static void exampleString() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Введите строку:");
+        String str = scanner.nextLine();
+
+        switch (str) {
+            case "1":
+                System.out.println("Вы ввели строкой число 1");
+                break;
+            case "2", "3":
+                System.out.println("Вы ввели строкой число 2 или 3");
+                break;
+            default:
+                System.out.println("Вы ввели что-то не понятное");
+        }
+
+        scanner.close();
+    }
+
+    private static void exampleInt() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Введите число:");
+        int number = scanner.nextInt();
+
+        switch (number) {
+            case 1:
+                System.out.println("Вы ввели число 1");
+                break;
+            case 2:
+                System.out.println("Вы ввели число 2");
+                break;
+            case 3:
+                System.out.println("Вы ввели число 3");
+                break;
+            case 4:
+                System.out.println("Вы ввели число 4");
+                break;
+            default:
+                System.out.println("Вы ввели неизвестное число");
+        }
+
+        scanner.close();
     }
 }
