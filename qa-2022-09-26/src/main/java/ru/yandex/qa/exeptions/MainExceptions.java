d9dfe72ef1cb0201fc45c61d83acedc2a4eab358
VladWild <33370139+VladWild@users.noreply.github.com>
2022-09-26 18:31:06 +0300

    qa 2022-09-26 starter module

---
+++ qa-2022-09-26/src/main/java/ru/yandex/qa/exeptions/MainExceptions.java
@@ -0,0 +1,8 @@
+package ru.yandex.qa.exeptions;
+
+public class MainExceptions {
+
+    public static void main(String[] args) {
+
+    }
+}
---
+++ qa-2022-09-26/src/main/java/ru/yandex/qa/tech_task_6/Main.java
@@ -0,0 +1,8 @@
+package ru.yandex.qa.tech_task_6;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+    }
+}
